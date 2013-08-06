d05211dafbbc97121a8e10dbde5da07838a02404
AmazonAWS <webmaster@localhost>
2013-08-06 10:00:58 +0000

    fixed update-tickets

--- scripts/update-tickets.sh
+++ scripts/update-tickets.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 current="$( cd "$( dirname "$0" )" && pwd )"
-updatetickets=$current/update-tickets.js
-echo $updatetickets
-#/usr/local/bin/node $current/update-tickets.js
+#updatetickets=$current/update-tickets.js
+#echo $updatetickets
+/usr/local/bin/node $current/update-tickets.js
