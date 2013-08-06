c687d5bfa2d6f3e37f7f3b8d1a816dddc9b42d27
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-06 11:56:06 +0200

    fixed update-tickets

--- scripts/update-tickets.sh
+++ scripts/update-tickets.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 current="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/usr/local/bin/node $current/update-tickets.js
+updatetickets=$current/update-tickets.js
+echo $updatetickets
+#/usr/local/bin/node $current/update-tickets.js
