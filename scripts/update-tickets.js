0c46a68e9d346ca836773361669715d18db8c515
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-08 09:46:53 +0200

    util now exports .util and .test

--- scripts/update-tickets.js
+++ scripts/update-tickets.js
@@ -13,7 +13,7 @@ var fs = require('fs');
 var path = require('path');
 var ATTicketAvail = require('../lib/services/at-ticket-avail.js').ATTicketAvail;
 var log = require('../lib/util/log.js');
-var util = require('../lib/util/util.js');
+var util = require('../lib/util/util.js').util;
 var atlas = require('../lib/services/config/atlas.js');
 var atlasDefaults = require('../lib/services/config/atlas-defaults.js');
 
