66c0dabcd5710b5af915fc9cd71abbb232c7789f
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-08 10:29:53 +0200

    added tests.js

---
+++ test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Run package tests.
+ * (C) 2013 TuiInnovation.
+ */
+
+// requires
+var testing = require('testing');
+
+
+/**
+ * Run all module tests.
+ */
+exports.test = function(callback)
+{
+	var tests = {};
+	var files = {};
+	//files['lib'] = [ 'app.js' ];
+	files['lib/io'] = [ 'ajax', 'parametrized-string', 'xml-reader' ];
+	files['lib/services'] = [ 'at-ticket-avail', 'fs-venue-search' ];
+	files['lib/util'] = [ 'util' ];
+	for (var path in files) {
+		files[path].forEach(function(file) {
+			tests[path + "/" + file] = require('./' + path + '/' + file + '.js').test;
+		});
+	}
+	testing.run(tests, 100000, callback);
+}
+
+// run tests if invoked directly
+if (__filename == process.argv[1])
+{
+	exports.test(testing.show);
+}
