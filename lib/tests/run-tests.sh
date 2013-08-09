6267ea6f29b95f39b59ad0d44c06958bb671faef
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-09 09:25:58 +0200

    removed nodeunit tests

--- lib/tests/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#current="$( cd "$( dirname "$0" )" && pwd )"
-
-../../node_modules/nodeunit/bin/nodeunit param-string-tests.js
-../../node_modules/nodeunit/bin/nodeunit util-tests.js
-../../node_modules/nodeunit/bin/nodeunit xmlreader-tests.js
-../../node_modules/nodeunit/bin/nodeunit at-ticket-avail-tests.js
-../../node_modules/nodeunit/bin/nodeunit ajax-tests.js
-../../node_modules/nodeunit/bin/nodeunit fs-venue-search-tests.js
