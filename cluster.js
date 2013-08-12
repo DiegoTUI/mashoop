904ebc9b5f215c55b9889108d4ea30fac7ed23dd
Alex Fernández <alex.fernandez@mediasmart.es>
2013-08-12 02:01:28 +0200

    Script for cluster mode.

---
+++ cluster.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Start in cluster mode.
+ * Adapted from http://www.godtic.com/blog/2013/07/27/modo-cluster-para-node-js/
+ * (C) 2013 TuiInnovation.
+ */
+
+// requires
+var cluster = require('cluster');
+var app = require("./lib/app.js");
+
+// globals
+var numCPUs = require('os').cpus().length;
+
+// init
+if (cluster.isMaster)
+{
+	// create one worker per CPU
+	for (var i = 0; i < numCPUs; i++)
+	{
+		cluster.fork();
+	}
+	cluster.on('exit', function(worker, code, signal)
+	{
+		console.log('worker ' + worker.process.pid + ' died');
+		cluster.fork();
+	});
+}
+else
+{
+	// start the server
+	app.startServer();
+}
+
