ac7a3897739c0f39eb704b9addb992182c6bf07f
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-10-01 18:40:02 +0200

    jslinted

--- test.js
+++ test.js
@@ -13,23 +13,22 @@ process.setMaxListeners(0);
 /**
  * Run all module tests.
  */
-exports.test = function(callback)
-{
+exports.test = function(callback) {
 	var tests = {};
 	var files = {};
-	files['lib'] = [ 'app', 'db' ];
+	files.lib = [ 'app', 'db' ];
 	files['lib/io'] = [ 'ajax', 'parametrized-string', 'xml-reader', 'json-formatter' ];
 	files['lib/services'] = [ 'at-ticket-avail', 'fs-venue-search', 'at-read-ticket-names'];
 	files['lib/util'] = [ 'core' ];
 	files['lib/batch'] = ['ticket-avail-parser'];
 	for (var path in files) {
-		files[path].forEach(function(file) {
-			tests[path + "/" + file] = require('./' + path + '/' + file + '.js').test;
-		});
+		for (var i=0; i<files[path].length; i++) {
+			var file = files[path][i];
+			tests[path + '/' + file] = require('./' + path + '/' + file + '.js').test;
+		}
 	}
-	
 	testing.run(tests, 100000, callback);
-}
+};
 
 // run tests if invoked directly
 if (__filename == process.argv[1])
