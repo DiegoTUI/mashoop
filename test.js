427061d609043771fab3dca8e48fd49f2bfad74c
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-12-13 10:00:56 +0100

    solved the failing weird test related to db.js

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ exports.test = function(callback) {
 	var files = {};
 	files.lib = [ 'app', 'db' ];
 	files['lib/io'] = [ 'ajax', 'parametrized-string', 'xml-reader', 'json-formatter' ];
-	files['lib/services'] = [ 'at-ticket-avail', 'fs-venue-search', 'at-read-ticket-names', 'at-hotel-list', 'at-hotel-detail', 'ms-hotel-list'];
+	files['lib/services'] = ['at-ticket-avail', 'fs-venue-search', 'at-read-ticket-names', 'at-hotel-list', 'at-hotel-detail', 'ms-hotel-list'];
 	files['lib/util'] = [ 'core' ];
 	files['lib/batch'] = ['ticket-avail-parser', 'hotel-parser'];
 	for (var path in files) {
